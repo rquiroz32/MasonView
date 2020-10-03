22657f58761146bd2374b0c94639ca32bc276578
rquiroz <rquiroz3290@gmail.com>
2020-10-03 00:37:36 -0400

    Nearly complete pwd gen, need to handle unexpected inputs from user, otherwise fully functional. Also need to clean up code, remove unused variables, comments, etc

--- Develop/script.js
+++ Develop/script.js
@@ -1,102 +1,148 @@
- /*# 03 JavaScript: Password Generator
+console.log("script loaded")
+alert("Please follow the instructions to determine characters and length for password, then press 'Generate'")
 
-Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
-
-If you are unfamiliar with special characters, take a look at [some examples](https://www.owasp.org/index.php/Password_special_characters).
-
-## Task
-
-```
-Create  password generator
-```
-
-## Requirements
-
-```
-Ask the user a series of questions
-a) length of password (numeric input)
-b) validate length is between 8 and 128.
-c) type of characters to include - special characters, uppercase, lowercase, numeric(series of yes/no confirms)
-d) Validate the user selected at least one character type.
-
-Any invalid response will terminate password generation and the user must start over.
-
-Once the user has entered valid responses, generate a password that meets their criteria and display it in the box.*/
-
-
-// Assignment Code
+// Assignment Code, prompt and confirms
 var generateBtn = document.querySelector("#generate");
-var pwdLength = prompt("Please enter in a whole number between 8 and 128 to determine password length.")
-var specialCharsQ = prompt("Would you like to include special characters? Type 'y' for yes and 'n' for no")
-var upperCaseQ = prompt("Would you like your password to include Upper Case characters? Type 'y' for yes and 'n' for no")
-var lowerCaseQ = prompt("Would you like to include Lower Case characters? Type 'y' for yes and 'n' for no")
-var numericQ = prompt("Would you like to include numeric characters? Type 'y' for yes and 'n' for no")
-var userPwd = { specCharInclude: '', upperCaseInclude: '', lowerCaseInclude: '', numericInclude: '' }
-var categoryMaxRpts = pwdLength / 2
-var randPwdCharLib = []
+var specialCharsQ = confirm("Would you like to include special characters? Press 'okay' for yes and 'cancel' for no.")
+var upperCaseQ = confirm("Would you like your password to include Upper Case characters? Press 'okay' for yes and 'cancel' for no.")
+var lowerCaseQ = confirm("Would you like to include Lower Case characters? Press 'okay' for yes and 'cancel' for no.")
+var numericQ = confirm("Would you like to include numeric characters? Press 'okay' for yes and 'cancel' for no.")
+var pwdLength = parseInt(prompt("Please enter in a whole number between 8 and 128 to determine password length."),10)
+var potentialPwdCharLib = []
+var finalPwd = []
+var count = 4
+var written = false
 
 
-// vars for obj properties in case the obj code doesn't work
-/*var specCharInclude 
-var upperCaseInclude 
-var lowerCaseInclude 
-var numericInclude*/
 
-// Build library of chars with arrays
+// Library of chars 
 var specCharLib = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
-var lowerCaseLLib = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-var upperCaseLib =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-var numberLib = [0,1,2,3,4,5,6,7,8,9]
+var lowerCaseLib = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseLib = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numberLib = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
-// rand # function for max amount of times a 
-var RandNumCatMax = Math.floor(Math.Random()* categoryMaxRpts)
+// BEGIN FUNCTION DEFINITIONS
+
+// Returns the value at a random index for an array  
+function randomIndexFucnt(arr) {
+  return  arr[Math.floor(Math.random() * Math.floor(arr.length-1))];
+    }
 
 
 
-//Valid password length check:
-pwdLengthCheck:
-if (pwdLength <= 7 || pwdLength >= 128){
 
-    alert("Please only enter a whole number between 8 and 128.");
-    break pwdLengthCheck;
-
-}
-
-else if (pwdLength >=8 || pwdLength <=128) {
- 
-  // Length meets min reqs, begin question checks
-  questionChecks:
-    if (specCharInclude.toLowerCase() === 'y') {
-
-      for (i = categoryMaxRpt; i < specCharLib.length; i++ )  {
-
-           x = specCharLib[Math.floor(Math.Random()* specCharLib.length - 1)]
-           randPwdCharLib.push(x)
-      }
-
-    
-
-}
-
-else if ()
-
- 
-
-// Begin password gen function definition
-function generatePassword(){
-
-
+// This will be used to build final pass word
+function finalPasswordGen() {
+ finalPwd += randomIndexFucnt(potentialPwdCharLib);
+ return finalPwd
 }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  while (written=== false){
+  var password = finalPasswordGen();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+  written = true
+  }
+}
+
+function addSpecChars() {
+  potentialPwdCharLib = potentialPwdCharLib.concat(specCharLib);
+  return potentialPwdCharLib
+}
+
+function addUpperCase() {
+   potentialPwdCharLib = upperCaseLib.concat(potentialPwdCharLib);
+}
+
+function addLowerCase() {
+   potentialPwdCharLib = lowerCaseLib.concat(potentialPwdCharLib);
+}
+
+function addNumLib() {
+   potentialPwdCharLib = numberLib.concat(potentialPwdCharLib);
+}
+
+//potentialPwdCharLib = potentialPwdCharLib.concat(specCharLib);
+//console.log("this is addSpecChars"+addSpecChars())
+//console.log("this is final pwd generator funct"+finalPasswordGen())
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////
+////                     BEGIN CHECKS                           ////
+///////////////////////////////////////////////////////////////////
+
+
+
+
+if (parseInt(pwdLength, 10) < 8 || parseInt(pwdLength, 10) > 128) {
+
+   alert("Please only enter a whole number between 8 and 128.");
+  location.reload();
 
 }
 
+
+else if (pwdLength >= 8 || pwdLength <= 128) {
+
+  
+  if (specialCharsQ) {
+    //concat array
+    addSpecChars();
+  }
+
+  else {
+    count--
+  }
+
+  if (upperCaseQ) {
+    //concat array
+    addUpperCase();
+  }
+
+  else {
+    count--
+  }
+
+  if (lowerCaseQ) {
+
+    //concat array
+    addLowerCase();
+  }
+
+  else {
+    count--
+  }
+
+
+  if (numericQ) {
+    //concat array
+    addNumLib();
+  }
+
+  else {
+    count--
+  }
+
+
+  if (count > 0) {
+    for (i=0; i<pwdLength-1; i++){
+      finalPasswordGen();
+      console.log("this is"+finalPwd)
+  }
+}
+
+  else {
+    alert("Please choose at least one category to build a password from");
+    location.reload();
+  }
+
+
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
